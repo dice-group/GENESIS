42cb51f63377edff1ddd8b7141347d41b21a79b6
Tim Ermilov <yamalight@gmail.com>
2016-04-20 17:04:58 +0200

    adapt to new lucene index

--- 3rd-party-services/search-lucene/src/main/kotlin/App.kt
+++ 3rd-party-services/search-lucene/src/main/kotlin/App.kt
@@ -9,7 +9,6 @@ import org.apache.lucene.index.IndexableField
 import org.apache.lucene.index.Term
 import org.apache.lucene.search.IndexSearcher
 import org.apache.lucene.search.BooleanQuery
-import org.apache.lucene.search.PhraseQuery
 import org.apache.lucene.search.PrefixQuery
 import org.apache.lucene.search.BooleanClause
 import org.apache.lucene.search.BooleanClause.Occur
@@ -17,8 +16,11 @@ import org.apache.lucene.search.TopDocs
 import com.google.gson.Gson
 import spark.Spark.get
 import spark.Spark.port
+import kotlin.comparisons.compareBy
 
 val TIMES_MORE_RESULTS = 10
+
+data class Result(val url: String, val label: String, val pagerank: Double)
 
 fun indexFromFolder(path: String): NIOFSDirectory {
     return NIOFSDirectory(File(path))
@@ -30,52 +32,48 @@ fun readerFromIndex(dir: NIOFSDirectory): DirectoryReader {
 
 fun queryFromString(queryString: String): BooleanQuery {
     val query = BooleanQuery()
-    val phraseQuery = PhraseQuery()
     val words = URLDecoder.decode(queryString, "UTF-8").toLowerCase().split(" ")
     words.map { it.trim() }
     .filter { !it.isEmpty() && it != "*" }
     .forEach{
-        phraseQuery.add(Term("label", it))
         val parse = PrefixQuery(Term("label", it))
-        parse.setBoost(0.9f)
         val clause = BooleanClause(parse, Occur.SHOULD)
         query.add(clause)
     }
     return query
 }
 
-fun search(index: NIOFSDirectory, queryString: String, limit: Int = 10): Array<Document> {
-    var reader = readerFromIndex(index)
+fun search(searcher: IndexSearcher, queryString: String, limit: Int = 10): List<Result> {
     val query = queryFromString(queryString)
-    val searcher = IndexSearcher(reader)
     val hits: TopDocs = searcher.search(query, limit * TIMES_MORE_RESULTS)
-    val res: Array<Document> = hits.scoreDocs
+    val res: List<Result> = hits.scoreDocs
         .map { searcher.doc(it.doc) }
         .fold (hashMapOf<String, Document>(), { acc, next ->
-            val uri: String = next.get("uri")
-            if(!acc.containsKey(uri)){
-                acc.put(uri, next)
+            val url: String = next.get("url")
+            if(!acc.containsKey(url)){
+                acc.put(url, next)
             }
             acc
         })
         .values
         .toTypedArray()
+        .map {
+            Result(url = it.get("url"), label = it.get("label"), pagerank = it.get("pagerank").toDouble())
+        }
+        .sortedWith(compareBy({it.pagerank}, {it.label}))
+        .asReversed()
     return res
 }
 
 fun main(args: Array<String>) {
     val index = indexFromFolder("./index")
+    var reader = readerFromIndex(index)
+    val searcher = IndexSearcher(reader)
     val gson = Gson()
     port(8181)
     get("/search", { req, res ->
         val q: String = req.queryParams("q")
-        val result: Array<Document> = search(index, q)
-        gson.toJson(result.map {
-            val fields: List<IndexableField> = it.getFields()
-            fields.fold (hashMapOf<String, String>(), { acc, f ->
-                acc.put(f.name(), f.stringValue())
-                acc
-            })
-        })
+        val result: List<Result> = search(searcher, q)
+        gson.toJson(result)
     })
 }
