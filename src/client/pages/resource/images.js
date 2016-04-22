71be95d0c196e9b12d503392a1ec645debcd2481
Tim Ermilov <yamalight@gmail.com>
2016-04-22 12:17:05 +0200

    add lightbox to images

--- src/client/pages/resource/images.js
+++ src/client/pages/resource/images.js
@@ -1,21 +1,50 @@
 import React from 'react';
+import Portal from 'react-portal';
 import styles from './resource.css';
 
-export default ({images}) => (
-    <div className="panel panel-default">
-        <div className="panel-heading">
-            Images
-        </div>
-        <div className={`panel-body ${styles.mediaPanel}`}>
-            {images.count() === 0 && 'No images available'}
-            {images.map(img => (
-                <a href={img} key={img}>
-                    <img src={img}
-                        alt={img}
-                        className={`img-responsive img-thumbnail ${styles.imageGrid}`}
-                    />
-                </a>
-            )) || 'No images available'}
-        </div>
-    </div>
-);
+const Images = React.createClass({
+    getInitialState() {
+        return {
+            showLightbox: false,
+        };
+    },
+
+    showImage(e, img) {
+        e.preventDefault();
+        this.setState({img, showLightbox: true});
+    },
+
+    render() {
+        const {images} = this.props;
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    Images
+                </div>
+                <div className={`panel-body ${styles.mediaPanel}`}>
+                    {images.count() === 0 && 'No images available'}
+                    {images.map(img => (
+                        <a href="#" onClick={(e) => this.showImage(e, img)} key={img}>
+                            <img
+                                src={img}
+                                alt={img}
+                                className={`img-responsive img-thumbnail ${styles.imageGrid}`}
+                            />
+                        </a>
+                    )) || 'No images available'}
+                </div>
+                <Portal closeOnEsc closeOnOutsideClick isOpened={this.state.showLightbox}>
+                    <div className={styles.lightbox} onClick={() => this.setState({showLightbox: false})}>
+                        <img
+                            className={`img-responsive ${styles.imageLightbox}`}
+                            alt={this.state.img}
+                            src={this.state.img}
+                        />
+                    </div>
+                </Portal>
+            </div>
+        );
+    },
+});
+
+export default Images;
